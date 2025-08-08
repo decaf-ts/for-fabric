6eae7d63d1775db963317644e574d76971af2816
bagacito <vnogueira91@gmail.com>
2025-08-08 10:50:41 +0100

    DECAF-168 - added base contract to test infrstructure

--- tests/integration/contract.test.ts
+++ tests/integration/contract.test.ts
@@ -1,26 +1,17 @@
 
-// import { min, minlength, model, Model, ModelArg, required, type } from "@decaf-ts/decorator-validation";
-// import { RamAdapter } from "../../src/ram/RamAdapter";
-// import { BaseModel, index, OrderDirection, pk, Repository, uses } from "../../src";
-// import { readonly } from "@decaf-ts/db-decorators";
-
-// import { execSync } from 'child_process';
-import {compileContract, compileStandaloneFile} from "../../src/compile"
-// Model.setBuilder(Model.fromModel);
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
 jest.setTimeout(5000000);
 
 describe("Test test model contract", () =>  {
 
     beforeAll(async () => {
-        // compile contract
-        // execSync(`weaver compile-contract -d --contract-path ./tests/assets/contract/test \
-        //     --contract-filename TestModelContract \
-        //     --contract-version 1.0.0 \
-        //     --tsconfig tsconfig.json \
-        //     --output-path ./infrastructure/chaincode`);
-    //    await compileContract("./tests/assets/contract/test", "TestModelContract", "1.0.0","./tsconfig-cc.json", "./docker/infrastructure/chaincode");
-    compileStandaloneFile("./tests/assets/contract/asset-tranfer/index.ts", "./docker/infrastructure/chaincode");
+        execSync(`npx weaver compile-contract -d --contract-file ./tests/assets/contract/asset-transfer/index.ts --output-dir ./docker/infrastructure/chaincode`);
+    
+        fs.copyFileSync(path.join(process.cwd(), "./tests/assets/contract/asset-transfer/package.json"), path.join(process.cwd(), "./docker/infrastructure/chaincode/package.json"));
+        fs.copyFileSync(path.join(process.cwd(), "./tests/assets/contract/asset-transfer/npm-shrinkwrap.json"), path.join(process.cwd(), "./docker/infrastructure/chaincode/npm-shrinkwrap.json"))
     });
 
 
@@ -29,79 +20,3 @@ describe("Test test model contract", () =>  {
         console.log("Validate a TestModel instance");
     })
 })
-
-// describe("Adapter Integration", () => {
-//   let adapter1: RamAdapter;
-//   let adapter2: RamAdapter;
-
-//   beforeAll(async () => {
-//     // First db
-//     adapter1 = new RamAdapter("db1");
-//     adapter2 = new RamAdapter("db2");
-
-
-//     // // Second DB
-//     // PouchDb.plugin(memoryAdapter);
-//     // const db2 = new PouchDb('db2', { adapter: 'memory' });
-//     // adapter2 = new PouchAdapter(db2,"db2");
-//   });
-//   @uses("ram")
-//   @model()
-//   class TestUser extends BaseModel {
-//     @pk({ type: "Number" })
-//     id!: number;
-
-//     @required()
-//     @min(18)
-//     @index([OrderDirection.DSC, OrderDirection.ASC])
-//     age!: number;
-
-//     @required()
-//     @minlength(5)
-//     name!: string;
-
-//     @required()
-//     @readonly()
-//     @type([String.name])
-//     sex!: "M" | "F";
-
-//     constructor(arg?: ModelArg<TestUser>) {
-//       super(arg);
-//     }
-//   }
-
-
-//   it("Create and read on multiple DBs", async () => {
-//     const repo1 = new Repository(adapter1,TestUser)
-    
-//     const model1 = new TestUser({
-//           age: 20,
-//           name: "User1" ,
-//           sex: "M",
-//         })
-
-//     const created1 = await repo1.create(model1);
-//     expect(created1).toBeDefined();
-//     expect(!created1.hasErrors()).toBe(true);
-
-//     const repo2 = new Repository(adapter2,TestUser)
-    
-//     const model2 = new TestUser({
-//           age: 21,
-//           name: "User2" ,
-//           sex: "F",
-//         })
-
-//     const created2 = await repo2.create(model2);
-//     expect(created2).toBeDefined();
-//     expect(!created2.hasErrors()).toBe(true);
-
-//     const result1 = await repo1.read(created1.id);
-//     expect(created1).toEqual(result1);
-
-//     const result2 = await repo2.read(created2.id);
-//     expect(created2).toEqual(result2);
-
-//   });
-// });
-
