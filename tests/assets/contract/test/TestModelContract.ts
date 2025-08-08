1a1ac720cc4eb7e027bd2ccba8a988d8a1355191
bagacito <vnogueira91@gmail.com>
2025-08-08 15:40:00 +0100

    DECAF-168 - base adjustements

--- tests/assets/contract/test/TestModelContract.ts
+++ tests/assets/contract/test/TestModelContract.ts
@@ -3,9 +3,11 @@ import { TestModel } from "./TestModel";
 import { FabricObject } from "../../../../src/shared/fabric-shims"
 import {Info} from "fabric-contract-api";
 import { BaseModel } from "@decaf-ts/core";
+import { Model} from "@decaf-ts/decorator-validation"
 
 FabricObject(TestModel)
 FabricObject(BaseModel)
+FabricObject(Model)
 @Info({title: 'TestContract', description: 'Test implementation of crud contract'})
 export class TestModelContract extends FabricCrudContract<TestModel> {
   constructor() {
