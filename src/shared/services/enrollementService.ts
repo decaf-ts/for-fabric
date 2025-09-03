68721aba742a84d51379ca18cbd84552f2886a58
tvenceslau <venceslau.tiago@gmail.com>
2025-09-03 10:47:48 +0100

    DECAF-168 - Refactor shared error classes and update error handling in Fabric implementation.
    Replaced unused custom error classes with `BaseError` from `@decaf-ts/db-decorators`. Updated configuration files and added lock files for TLS and Fabric CA client setup.

--- src/shared/services/enrollementService.ts
+++ src/shared/services/enrollementService.ts
@@ -13,8 +13,11 @@ import { Identity } from "../../shared/model/Identity";
 import { stringFormat } from "@decaf-ts/decorator-validation";
 import { Logger, Logging, MiniLogger } from "@decaf-ts/logging";
 import { AuthorizationError } from "@decaf-ts/core";
-import { ConflictError, NotFoundError } from "@decaf-ts/db-decorators";
-import { DLTError, RegistrationError } from "../errors";
+import {
+  ConflictError,
+  InternalError,
+  NotFoundError,
+} from "@decaf-ts/db-decorators";
 import { CoreUtils } from "../utils";
 import { CA_ROLE } from "./constants";
 import { CryptoUtils } from "../crypto";
@@ -24,6 +27,7 @@ import {
   GetCertificatesRequest,
   IdentityResponse,
 } from "../fabric-shims";
+import { RegistrationError } from "../errors";
 
 export enum HFCAIdentityType {
   PEER = "peer",
@@ -147,41 +151,41 @@ export class FabricEnrollmentService {
   ): Promise<string[] | CertificateResponse> {
     const certificateService = await this.Certificate();
     const user = await this.User();
-    this.logger.debug(
-      stringFormat(
-        "Retrieving certificates {0} for CA {1}",
-        request ? stringFormat("for {0}", request.id as string) : "",
-        this.caConfig.caName
-      )
-    );
+    // this.logger.debug(
+    //   stringFormat(
+    //     "Retrieving certificates {0} for CA {1}",
+    //     request ? stringFormat("for {0}", request.id as string) : "",
+    //     this.caConfig.caName
+    //   )
+    // );
     const response: CertificateResponse = (
       await certificateService.getCertificates(request || {}, user)
     ).result;
-    this.logger.debug(
-      stringFormat(
-        "Found {0} certificates: {1}",
-        response.certs.length + "",
-        JSON.stringify(response)
-      )
-    );
+    // this.logger.debug(
+    //   stringFormat(
+    //     "Found {0} certificates: {1}",
+    //     response.certs.length + "",
+    //     JSON.stringify(response)
+    //   )
+    // );
     return doMap ? response.certs.map((c) => c.PEM) : response;
   }
 
   async getIdentities(): Promise<FabricIdentity[]> {
     const identitiesService = await this.Identities();
-    this.logger.debug(
-      stringFormat("Retrieving Identities under CA {0}", this.caConfig.caName)
-    );
+    // this.logger.debug(
+    //   stringFormat("Retrieving Identities under CA {0}", this.caConfig.caName)
+    // );
     const response: IdentityResponse = (
       await identitiesService.getAll(await this.User())
     ).result;
-    this.logger.debug(
-      stringFormat(
-        "Found {0} Identities: {1}",
-        response.identities.length + "",
-        JSON.stringify(response)
-      )
-    );
+    // this.logger.debug(
+    //   stringFormat(
+    //     "Found {0} Identities: {1}",
+    //     response.identities.length + "",
+    //     JSON.stringify(response)
+    //   )
+    // );
     return response.identities;
   }
 
@@ -203,18 +207,18 @@ export class FabricEnrollmentService {
 
   async getAffiliations() {
     const affiliationService = await this.Affiliations();
-    this.logger.debug(
-      stringFormat("Retrieving Affiliations under CA {0}", this.caConfig.caName)
-    );
+    // this.logger.debug(
+    //   stringFormat("Retrieving Affiliations under CA {0}", this.caConfig.caName)
+    // );
     const response = (await affiliationService.getAll(await this.User()))
       .result;
-    this.logger.debug(
-      stringFormat(
-        "Found {0} Affiliations: {1}",
-        response.a.length + "",
-        JSON.stringify(response)
-      )
-    );
+    // this.logger.debug(
+    //   stringFormat(
+    //     "Found {0} Affiliations: {1}",
+    //     response.a.length + "",
+    //     JSON.stringify(response)
+    //   )
+    // );
     return response;
   }
 
@@ -266,13 +270,13 @@ export class FabricEnrollmentService {
         // maxEnrollments: (role === CA_ROLE.ADMIN || isSuperUser) ? -1 : 1
       } as IRegisterRequest;
       registration = await ca.register(props, user);
-      this.logger.info(
-        stringFormat(
-          `Registration for {0} created with user type {1} ${isSuperUser ? "as super user" : ""} `,
-          model.userName as string,
-          userRole ?? "Undefined Role"
-        )
-      );
+      // this.logger.info(
+      //   stringFormat(
+      //     `Registration for {0} created with user type {1} ${isSuperUser ? "as super user" : ""} `,
+      //     model.userName as string,
+      //     userRole ?? "Undefined Role"
+      //   )
+      // );
     } catch (e: any) {
       throw this.parseError(e);
     }
@@ -285,16 +289,16 @@ export class FabricEnrollmentService {
   ): Identity {
     const { certificate, key, rootCertificate } = enrollment;
     const logger = Logging.for(FabricEnrollmentService);
-    logger.debug(
-      stringFormat(
-        "Generating Identity from certificate {0} in msp {1}",
-        certificate,
-        mspId
-      )
-    );
+    // logger.debug(
+    //   stringFormat(
+    //     "Generating Identity from certificate {0} in msp {1}",
+    //     certificate,
+    //     mspId
+    //   )
+    // );
     const clientId = CryptoUtils.fabricIdFromCertificate(certificate);
     const id = CryptoUtils.encode(clientId);
-    logger.debug(stringFormat("Identity {0} and encodedId {1}", clientId, id));
+    // logger.debug(stringFormat("Identity {0} and encodedId {1}", clientId, id));
     const now = new Date();
     return new Identity({
       id: id,
@@ -391,12 +395,12 @@ export class FabricEnrollmentService {
         user
       );
     } catch (e: any) {
-      throw new DLTError(
+      throw new InternalError(
         stringFormat("Could not revoke enrollment with id {0}", enrollmentId)
       );
     }
     if (!result.success)
-      throw new DLTError(
+      throw new InternalError(
         stringFormat(
           "Could not revoke enrollment with id {0}:\n{1}",
           enrollmentId,
