005c04aa209ce40c8508f3dd14df6c766a55d5a5
tvenceslau <venceslau.tiago@gmail.com>
2025-11-25 14:33:07 +0000

    DECAF-178 - remove unused test files and refactor imports to clean up test suite

--- tests/unit/Serialized-Contract-Public-Model.test.ts
+++ tests/unit/Serialized-Contract-Public-Model.test.ts
@@ -1,15 +1,6 @@
-import { MiniLogger } from "@decaf-ts/logging";
 import { TestPublicModelContract } from "../assets/contract/serialized-contract-public-model/TestPublicModelContract";
 import { TestPublicModel } from "../assets/contract/serialized-contract-public-model/TestPublicModel";
-import { getMockCtx } from "./Context.mock";
-
-const logger = new MiniLogger("public-model-test") as any;
-if (typeof logger.for !== "function") {
-  logger.for = jest.fn().mockReturnValue(logger);
-}
-if (typeof logger.clear !== "function") {
-  logger.clear = jest.fn().mockReturnValue(logger);
-}
+import { getMockCtx } from "./ContextMock";
 
 describe("Tests Public contract", () => {
   const ctx = getMockCtx();
