34df08ab6a83151e2b1fbb82e07af7247e47ed6f
tvenceslau <venceslau.tiago@gmail.com>
2025-12-17 11:26:54 +0000

    patch - DECAF-204 updates-test-release-no-ci - after release preparation

---
+++ tests/assets/contract/serialized-contract-public-model/TestPublicModel.ts
@@ -0,0 +1,35 @@
+import { column, pk, table } from "@decaf-ts/core";
+import {
+  maxlength,
+  minlength,
+  Model,
+  model,
+  ModelArg,
+  required,
+} from "@decaf-ts/decorator-validation";
+import { OwnedBy } from "../../../../src/shared/decorators";
+
+@table("tst_user")
+@model()
+export class TestPublicModel extends Model {
+  @pk({ type: "Number" })
+  id!: number;
+
+  @column("tst_name")
+  @required()
+  name!: string;
+
+  @column("tst_nif")
+  // @unique()
+  @minlength(9)
+  @maxlength(9)
+  @required()
+  nif!: string;
+
+  @OwnedBy()
+  owner!: string;
+
+  constructor(arg?: ModelArg<TestPublicModel>) {
+    super(arg);
+  }
+}
