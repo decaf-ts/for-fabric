9e181c9ec2953802c93b3430834be0042b73e7fa
tvenceslau <venceslau.tiago@gmail.com>
2026-01-20 14:58:11 +0000

    patch - DECAF-206 fixes default flags in fabric client-no-ci - after release preparation

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -1,12 +1,8 @@
 import { FabricClientFlags } from "./types";
-import { DefaultAdapterFlags } from "@decaf-ts/core";
 
-export const DefaultFabricClientFlags: FabricClientFlags = Object.assign(
-  {
-    evaluateTimeout: 5,
-    endorseTimeout: 15,
-    submitTimeout: 5,
-    commitTimeout: 60,
-  },
-  DefaultAdapterFlags
-) as any;
+export const DefaultFabricClientFlags: FabricClientFlags = Object.assign({
+  evaluateTimeout: 5,
+  endorseTimeout: 15,
+  submitTimeout: 5,
+  commitTimeout: 60,
+}) as any;
