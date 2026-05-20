54e20580eb21c437bca3e14434db8ad760981ada
tvenceslau <venceslau.tiago@gmail.com>
2026-05-20 14:56:44 +0100

    DECAF-805 - alias flavour

--- src/contract/models/History.ts
+++ src/contract/models/History.ts
@@ -31,7 +31,7 @@ import {
 @model()
 export class History extends Model {
   @pk()
-  @mirror("ptp-history-mirror", "PharmaledgerassocMSP")
+  @mirror("ptp-history-mirror", "org-b")
   @composed(["table", "key", "version"], ":")
   @description("Unique identifier of the history record.")
   id!: string;
